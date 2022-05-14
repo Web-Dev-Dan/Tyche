4b7b54d73b3affda917337787c759db29ef79102
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2022-05-14 20:21:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 
+'use strict';
+
+// ----- 💬 Notifications 💬 -----
+const mainContainer = document.getElementById('mainContainer');
+const updatesContainer = document.getElementById('updates');
+const closeUpdatesBtn = document.getElementById('closeUpdatesBtn');
+
+const closeUpdatesContainer = () => {
+    updatesContainer.remove();
+}
+
+closeUpdatesBtn.addEventListener('click', closeUpdatesContainer);
+
+
+// ----- 🙍‍♂️ User Button 🙎‍♀️ -----
+const anonymousUserBtn = document.getElementById('anonymousUserBtn');
+const loginDetail = document.getElementById('loginDetail');
+const closeLoginDetailBtn = document.getElementById('closeLoginDetailBtn');
+
+const openLoginDetail = () => {
+    mainContainer.classList.add('element-hidden');
+    loginDetail.classList.remove('element-hidden');
+}
+
+const closeLoginDetail = () => {
+    mainContainer.classList.remove('element-hidden');
+    loginDetail.classList.add('element-hidden');
+}
+
+anonymousUserBtn.addEventListener('click', openLoginDetail);
+closeLoginDetailBtn.addEventListener('click', closeLoginDetail);
+
+// ----- 🙍‍♂️ Login Button 🙎‍♀️ -----
+const loginBtn = document.getElementById('loginBtn');
+
+const login = (e) => {
+    e.preventDefault();
+    console.log('Logging in...');
+}
+
+loginBtn.addEventListener('click', login);
+
+
+let loggedIn = false;
